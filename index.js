4bfd3ecce9ebccc6ce802aed4a1a43c4537b3238
inikulin <ifaaan@gmail.com>
2014-11-26 17:02:08 +0300

    Start grid transpiler implementation

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var ineed = require('ineed');
+var ineed = require('ineed'),
+    GridTranspiler = require('./lib/grid_transpiler');
 
-var reprocess = ineed.reprocess;
+var reprocess = ineed
+    .using(GridTranspiler)
+    .reprocess
+    .grid();
 
 exports.compile = function (html) {
     return reprocess.fromHtml(html);
